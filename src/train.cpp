8abc540927eac98f777219f8fdc83a178068bd56
letmebleedout <199362693+letmebleedout@users.noreply.github.com>
2025-05-07 14:42:19 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,47 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train()
+  : first(nullptr), countOp(0)
+{}
+Train::~Train() {
+  if (!first) return;
+  Car* cur = first->next;
+  while (cur != first) {
+    Car* tmp = cur;
+    cur = cur->next;
+    delete tmp;
+  }
+  delete first;
+}
+void Train::addCar(bool light) {
+  Car* car = new Car{ light, nullptr, nullptr };
+  if (!first) {
+    car->next = car;
+    car->prev = car;
+    first = car;
+  } else {
+    Car* last = first->prev;
+    last->next = car;
+    car->prev = last;
+    car->next = first;
+    first->prev = car;
+  }
+}
+int Train::getLength() {
+  if (!first) return 0;
+  countOp = 0;
+  bool init = first->light;
+  first->light = !init;
+  Car* cur = first->next;
+  ++countOp;
+  while (cur->light != !init) {
+    cur = cur->next;
+    ++countOp;
+  }
+  cur->light = init;
+  return countOp;
+}
+int Train::getOpCount() const {
+  return countOp;
+}
